7863d6323fdcd1736699ea5ddaa6c9458e6ee2b8
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-05-14 10:26:21 -0300

    Change database to Mysql.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/flask-crud'
 
 login_manager = LoginManager()
 
